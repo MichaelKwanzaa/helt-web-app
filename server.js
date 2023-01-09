e8d8308b313b4fa142c2b8044708c20c45f6b1a6
Michael Agbonyeme <66803740+MichaelKwanzaa@users.noreply.github.com>
2023-01-09 17:59:01 +0000

    increased payload

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 connectToDB();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
 app.use(helmet());
 app.use(morgan("combined"));
 app.use(function(req, res, next){
